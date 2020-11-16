e7e49cb46186c934914c1215d99c72099cda7661
alicia-klln <57350795+alicia-klln@users.noreply.github.com>
2020-11-16 22:41:56 +0100

    made login possible

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::resource('events', EventController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
