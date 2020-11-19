bf59b3512f609f7e4afbcd36a777c73990001336
alicia-klln <57350795+alicia-klln@users.noreply.github.com>
2020-11-19 10:46:14 +0100

    changed index style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,39 +1,34 @@
 
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold  text-gray-800 leading-tight">
+        <h2 class="">
             {{ __('Deine Übersicht') }}
         </h2>
     </x-slot>
 
     <div class="container-fluid">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="row justify-content-center mt-3">
-                    <div class="col-6 bg-light py-3">
-                        <h3>Hey {{ Auth::user()->name }}, </h3>
-                        <h3><strong>Das ist dein persönlicher Bereich!</strong></h3>
-                        Hier findest du eine Liste deiner Termine.
+        <div class="row justify-content-center text-center mt-5">
+            <div class="col-8 bg-dark text-light py-3 rounded">
+                <h3>Willkommen {{ Auth::user()->name }}! </h3>
+                <h5>Das ist die Übersichtsseite deines persönlichen Bereiches.</h5>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+            <div class="col-3">
+                <div class="card bg-dark text-white" style="height: 200px">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">Terminübersicht</h4> 
+                            <p class="card-text" style="height: 50px">Hier siehst du eine Übersicht aller Termine.</p>
+                            <a href="{{ route('events.index') }}" class="btn btn-outline-light">Zu deinen Terminen</a>
+                        </div>
                     </div>
                 </div>
-                <div class="row justify-content-center mt-3">
-                    <div class="col-3">
-                        <div class="card" style="height: 200px">
-                            <h4 class="card-header">Terminübersicht</h4> 
-                            <div class="card-body text-center">
-                                <h5 class="card-text" style="height: 50px">Hier siehst du eine Übersicht aller Termine.</h5>
-                                <a href="{{ route('events.index') }}" class="btn btn-secondary btn-lg">Zu deinen Terminen</a>
-                            </div>
-                            </div>
-                        </div>
-                    <div class="col-3">
-                        <div class="card" style="height: 200px">
-                        <h4 class="card-header">Neuer Termin</h4>    
-                        <div class="card-body text-center">
-                            <h5 class="card-text" style="height: 50px">Erstelle direkt einen neuen Termin.</h5>
-                            <a href="{{ route('events.create') }}" class="btn btn-secondary btn-lg">Neuen Termin erstellen</a>
-                        </div>
-                        </div>
+            <div class="col-3">
+                <div class="card bg-dark text-white" style="height: 200px">
+                    <div class="card-body text-center">
+                    <h4 class="card-title">Neuer Termin</h4> 
+                        <p class="card-text" style="height: 50px">Erstelle direkt einen neuen Termin.</p>
+                        <a href="{{ route('events.create') }}" class="btn btn-outline-light">Neuen Termin erstellen</a>
                     </div>
                 </div>
             </div>
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -15,37 +15,43 @@
     @endif
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-8">
-                <table class="table table-bordered">
-                    <tr>
-                        <th>Event-ID</th>
-                        <th>Name</th>
-                        <th>Ersteller</th>
-                        <th>Details</th>
-                        <th width="280px">Action</th>
-                    </tr>
-                    @foreach ($events as $event)
-                    <tr>
-                        <td style="width: 100px">{{ $event->id }}</td>
-                        <td style="width: 150px">{{ $event->name }}</td>
-                        <td style="width: 100px">{{ $event->creater }}</td>
-                        
-                        <td style="width: 600px">{{ $event->detail }}</td>
-                        <td class="text-center">
-                            <form action="{{ route('events.destroy',$event->id) }}" method="POST">
-            
-                                <button class="btn btn-info" href="{{ route('events.show',$event->id) }}" style=" margin: 2px">Ansehen</button>
+            <div class="col-8 table-responsive">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Event-ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Ersteller</th>
+                            <th scope="col">Details</th>
+                            <th scope="col" width="280px">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($events as $event)
+                            @if ($users()->name == $creater)
+                        <tr>
+                            <td style="width: 100px">{{ $event->id }}</td>
+                            <td style="width: 150px">{{ $event->name }}</td>
+                            <td style="width: 100px">{{ $event->creater }}</td>
+                            
+                            <td style="width: 600px">{{ $event->detail }}</td>
+                            <td class="text-center">
+                                <form action="{{ route('events.destroy',$event->id) }}" method="POST">
                 
-                                <button class="btn btn-secondary" href="{{ route('events.edit',$event->id) }}" style="margin: 2px">Bearbeiten</button>
-            
-                                @csrf
-                                @method('DELETE')
+                                    <button class="btn btn-info" href="{{ route('events.show',$event->id) }}" style=" margin: 2px">Ansehen</button>
+                    
+                                    <button class="btn btn-secondary" href="{{ route('events.edit',$event->id) }}" style="margin: 2px">Bearbeiten</button>
                 
-                                <button type="submit" class="btn btn-danger" style="margin: 2px">Löschen</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
+                                    @csrf
+                                    @method('DELETE')
+                    
+                                    <button type="submit" class="btn btn-danger" style="margin: 2px">Löschen</button>
+                                </form>
+                            </td>
+                        </tr>
+                            @endif
+                        @endforeach
+                    </tbody>
                 </table>
                 <button class="btn btn-secondary btn-lg" href="{{ route('events.create') }}">Neuer Termin</button>
             </div>
